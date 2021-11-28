2242f9b3ea1c7a52550c71ff22925c4d8c9db2b3
takaaki <taka.morimitsu@gmail.com>
2021-11-28 17:14:52 +0900

    家計簿作成

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,137 @@
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+/** State Vuexの状態 */
+const state = {
+  /** 設定 */
+  settings: {
+    appName: 'GAS 家計簿',
+    apiUrl: '',
+    authToken: '',
+    strIncomeItems: '給料, ボーナス, 繰越',
+    strOutgoItems: '食費, 趣味, 交通費, 買い物, 交際費, 生活費, 住宅, 通信, 車, 税金',
+    strTagItems: '固定費, カード'
   },
-  mutations: {
+  /** 家計簿データ */
+  abData: {},
+
+  /** ～ 省略 ～ */
+}
+
+/** Mutations ActionsからStateを更新するときに呼ばれます */
+const mutations = {
+  /** 設定を保存します */
+  saveSettings(state, { settings }) {
+    state.settings =  { ...settings }
+    document.title = state.settings.appName
+    localStorage.setItem('settings', JSON.stringify(settings))
   },
-  actions: {
+  loadSettings(state) {
+    const settings = JSON.parse(localStorage.getItem('settings'))
+    if (settings) {
+      state.settings = Object.assign(state.settings, settings)
+    }
+    document.title = state.settings.appName
   },
-  modules: {
+  /** 指定年月の家計簿データをセットします */
+  setAbData(state, { yearMonth, list }) {
+    state.abData[yearMonth] = list
+  },
+  /** 家計簿データを追加します */
+  addAbData(state, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const list = state.abData[yearMonth]
+    if (list) {
+      list.push(item)
+    }
+  },
+  updateAbData(state, { yearMonth, item }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === item.id)
+      list.splice(index, 1, item)
+    }
+  },
+  /** 指定年月&IDのデータを削除します */
+  deleteAbData(state, { yearMonth, id }) {
+    const list = state.abData[yearMonth]
+    if (list) {
+      const index = list.findIndex(v => v.id === id)
+      list.splice(index, 1)
+    }
   }
+}
+
+/** Actions 画面から呼ばれ、Mutationをコミットします */
+const actions = {
+  /** 設定を保存します */
+  saveSettings({ commit }, { settings }) {
+    commit('saveSettings', { settings })
+  },
+  loadSettings({ commit }) {
+    commit('loadSettings')
+  },
+  /** 指定年月の家計簿データを取得します */
+  fetchAbData({ commit }, { yearMonth }) {
+    // サンプルデータを初期地として入れる
+    const list = [
+      { id: 'a341093e', date: `${yearMonth}-01`, title: '支出サンプル', category: '買い物', tags: 'タグ1', income: null, outgo: 2000, memo: 'メモ' },
+      { id: '7c8fa764', date: `${yearMonth}-02`, title: '収支サンプル', category: '給料', tags: 'タグ1,タグ2', income: 2000, outgo: null, memo: 'メモ' }
+    ]
+    commit('setAbData', { yearMonth, list })
+  },
+  /** データを追加します */
+  addAbData({ commit }, { item }) {
+    commit('addAbData', { item })
+  },
+  /** データを更新します */
+  updateAbData({ commit }, { beforeYM, item }) {
+    const yearMonth = item.date.slice(0, 7)
+    // 更新前後で年月の変更がなければ、そのままの値で更新
+    if (yearMonth === beforeYM) {
+      commit('updateAbData', { yearMonth, item })
+      return
+    }
+    // 更新があれば、更新前年月のデータから削除して新しくデータを追加する
+    const id = item.id;
+    commit('deleteAbData', { yearMonth: beforeYM, id })
+    commit('addAbData', { item })
+  },
+  /** データを削除します */
+  deleteAbData({ commit }, { item }) {
+    const yearMonth = item.date.slice(0, 7)
+    const id = item.id;
+    commit('deleteAbData', { yearMonth, id })
+  }
+}
+
+/** カンマ区切りの文字をトリミングして配列にします */
+const createItems = v => v.split(',').map(v => v.trim()).filter(v => v.length !== 0)
+
+/** Getters 画面から取得され、Stateを加工して渡します */
+const getters = {
+  /** 収支カテゴリ（配列） */
+  incomeItems(state) {
+    return createItems(state.settings.strIncomeItems)
+  },
+  /** 支出カテゴリ（配列） */
+  outgoItems(state) {
+    return createItems(state.settings.strOutgoItems)
+  },
+  /** タグ（配列） */
+  tagItems(state) {
+    return createItems(state.settings.strTagItems)
+  }
+}
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 })
+
+export default store
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,214 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-card>
+      <v-card-title>
+        <!-- 月選択 -->
+        <v-col cols="8">
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="yearMonth"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="yearMonth"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-on="on"
+                hide-details
+              />
+            </template>
+            <v-date-picker
+              v-model="yearMonth"
+              type="month"
+              color="green"
+              locale="ja-jp"
+                no-title
+                scrollable
+            >
+              <v-spacer/>
+              <v-btn text color="grey" @click="menu = false">キャンセル</v-btn>
+              <!-- <v-btn text color="primary" @click="$refs.menu.save(yearMonth)">選択</v-btn> -->
+              <v-btn text color="primary" @click="onSelectMonth">選択</v-btn>
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-spacer/>
+        <!-- 追加ボタン -->
+        <v-col class="text-right" cols="4">
+          <v-btn dark color="green" @click="onClickAdd">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </v-col>
+        <!-- 検索フォーム -->
+        <v-col cols="12">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="search"
+            single-line
+            hide-details
+          />
+        </v-col>
+      </v-card-title>
+      <!-- テーブル -->
+      <v-data-table
+        class="text-no-wrap"
+        :headers="tableHeaders"
+        :items="tableData"
+        :search="search"
+        :footer-props="footerProps"
+        :loading="loading"
+        :sort-by="'date'"
+        :sort-desc="true"
+        :items-per-page="30"
+        mobile-breakpoint="0"
+      >
+        <!-- 日付列 -->
+        <template v-slot:[`item.date`]="{ item }">
+          {{ parseInt(item.date.slice(-2)) + '日' }}
+        </template>
+        <!-- タグ列 -->
+        <template v-slot:[`item.tags`]="{ item}">
+          <div v-if="item.tags">
+            <v-chip
+              class="mr-2"
+              v-for="(tag, i) in item.tags.split(',')"
+              :key="i"
+            >
+              {{ tag }}
+            </v-chip>
+          </div>
+        </template>
+        <!-- 収入列 -->
+        <template v-slot:[`item.income`]="{ item }">
+          {{ separate(item.income) }}
+        </template>
+        <!-- 支出列 -->
+        <template v-slot:[`item.outgo`]="{ item }">
+          {{ separate(item.outgo) }}
+        </template>
+        <!-- 操作列 -->
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon class="mr-2" @click="onClickEdit(item)">mdi-pencil</v-icon>
+          <v-icon class="mr-2" @click="onClickDelete(item)">mdi-delete</v-icon>
+        </template>
+      </v-data-table>
+    </v-card>
+    <!-- 追加／編集ダイアログ -->
+    <item-dialog ref="itemDialog"/>
+    <!-- 削除ダイアログ -->
+    <delete-dialog ref="deleteDialog"/>
   </div>
-</template>
+</template>   
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex'
+import ItemDialog from '../components/ItemDialog.vue'
+import DeleteDialog from '../components/deleteDialog.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      ItemDialog,
+      DeleteDialog,
+    },
+    data() {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = ('0' + (today.getMonth() + 1)).slice(-2);
+
+      return {
+        /**  ローディング状態 */
+        loading: false,
+        /** 月選択メニューの状態 */
+        menu: false,
+        /** 検索文字 */
+        search: '',
+        /** 選択年月 */
+        yearMonth: `${year}-${month}`,
+        /** テーブルに表示させるデータ */
+        tableData: [
+          /** サンプルデータ */
+          //{ id: 'a34109ed', date: '2020-06-01', title: '支出サンプル', category: '買い物', tags: 'タグ1', income: null, outgo: 2000, memo: 'メモ'},
+          //{ id: '7c8fa764', date: '2020-06-02', title: '収入サンプル', category: '給料', tags: 'タグ1,タグ2', income: 2000, outgo: null, memo: 'メモ'},
+        ]
+      }
+    },
+    computed: {
+      ...mapState({
+        /** 家計簿データ */
+        abData: state => state.abData
+      }),
+      /** テーブルヘッダ設定 */
+      tableHeaders() {
+        return [
+          { text: '日付', value: 'date', align: 'end' },
+          { text: 'タイトル', value: 'title', sortable: false },
+          { text: 'カテゴリ', value: 'category', sortable: false },
+          { text: 'タグ', value: 'tags', sortable: false },
+          { text: '収入', value: 'income', align: 'end' },
+          { text: '支出', value: 'outgo', align: 'end' },
+          { text: 'メモ', value: 'memo', sortable: false },
+          { text: '操作', value: 'actions', sortable: false },
+        ]
+      },
+      footerProps() {
+        return { itemsPerPageText: '', itemsPerPageOptions: [] }
+      }
+    },
+    methods: {
+      /**
+       * 数字を3桁区切りにして返します。
+       * 受け取った数が null のときは null を返します。
+       */
+      separate(num) {
+        return num !== null ? num.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') : null
+      },
+      /** 追加ボタンがクリックされたとき */
+      onClickAdd() {
+        this.$refs.itemDialog.open('add');
+      },
+      /** 編集ボタンがクリックされたとき */
+      onClickEdit(item) {
+        this.$refs.itemDialog.open('edit', item);
+      },
+      onClickDelete(item) {
+        this.$refs.deleteDialog.open(item);
+      },
+      ...mapActions([
+        /** 家計簿データを取得 */
+        /** 
+         * this.$store.dispatch('fetchAbData')を
+         * this.fetchAbDataとして使えるようにする
+        */
+        'fetchAbData'
+      ]),
+      /** 表示させるデータを更新します */
+      updateTable() {
+        const yearMonth = this.yearMonth;
+        const list = this.abData[yearMonth];
+        if (list) {
+          this.tableData = list
+        } else {
+          this.fetchAbData({ yearMonth });
+          this.tableData = this.abData[yearMonth];
+        }
+      },
+      // 月選択ボタンがクリックされたとき
+      onSelectMonth() {
+        this.$refs.menu.save(this.yearMonth);
+        this.updateTable();
+      }
+    },
+    created() {
+      this.updateTable();
+    }
+
   }
-}
 </script>
